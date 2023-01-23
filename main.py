a85fb51039f49079c9261e5a5adbcdc6bb3cbdee
nganguly0594 <nganguly@stu.naperville203.org>
2023-01-22 21:22:55 -0600

    es

--- main.py
+++ main.py
@@ -1,28 +1,70 @@
 import time
+from pathlib import Path
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras.utils import image_dataset_from_directory
+from keras.callbacks import ModelCheckpoint
 from model import create_model
 from data_creator import download_flickr_data
+
 
 start = time.time()
 
 #Downloading the data
 #download_flicker_data()
 
+#Splitting the datasets
+data = Path('images')
+
+train_ds = image_dataset_from_directory(
+    data,
+    seed=69420,
+    validation_split=0.2,
+    subset='training'
+)
+
+val_ds = image_dataset_from_directory(
+    data,
+    seed=69420,
+    validation_split=0.2,
+    subset='validation'
+)
+
+at = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=at)
+val_ds = val_ds.cache().prefetch(buffer_size=at)
+
 #Creating and training the model
-model_history = create_model(50)
+model = create_model()
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+checkpoint_dir = Path.cwd() / f'trained models/mod1.ckpt'
+callback = ModelCheckpoint(
+    filepath=checkpoint_dir,
+    save_weights_only=True,
+    verbose=1
+)
+
+history = model.fit(train_ds, epochs=5, validation_data=val_ds, callbacks=[callback])
 
 #Plotting accuracy
 plt.subplot(1, 2, 1)
-plt.plot(model_history.history['accuracy'], label='Training Accuracy')
-plt.plot(model_history.history['val_accuracy'], label='Validation Accuracy')
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.ylim([0, 1])
 plt.legend(loc='lower right')
 
 plt.subplot(1, 2, 2)
-plt.plot(model_history.history['loss'], label='Training Loss')
-plt.plot(model_history.history['val_loss'], label='Validation Loss')
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.ylim([0, 1])
--- model.py
+++ model.py
@@ -1,33 +1,9 @@
-import tensorflow as tf
-from pathlib import Path
-from keras.utils import image_dataset_from_directory
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, MaxPooling2D, Conv2D, Rescaling
 
 #USE TF LITE FOR RASPBERRY PI
 
-def create_model(epochs):
-    data = Path('images')
-
-    train_ds = image_dataset_from_directory(
-        data,
-        seed=69420,
-        validation_split=0.2,
-        subset='training'
-    )
-
-    val_ds = image_dataset_from_directory(
-        data,
-        seed=69420,
-        validation_split=0.2,
-        subset='validation'
-    )
-
-    at = tf.data.AUTOTUNE
-
-    train_ds = train_ds.cache().prefetch(buffer_size=at)
-    val_ds = val_ds.cache().prefetch(buffer_size=at)
-
+def create_model():
     model = Sequential()
 
     model.add(Rescaling(1.0/255))
@@ -41,10 +17,4 @@ def create_model(epochs):
     model.add(Dense(64, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
 
-    model.compile(
-        optimizer='adam',
-        loss='binary_crossentropy',
-        metrics=['accuracy']
-    )
-
-    return model.fit(train_ds, epochs=epochs, validation_data=val_ds)
+    return model
