8ebd6f7a341f836408a4dae3a1ebb532cb033357
nganguly0594 <nganguly@stu.naperville203.org>
2023-01-23 16:32:02 -0600

    oef

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from keras.utils import image_dataset_from_directory
-from keras.callbacks import ModelCheckpoint
-from model import create_model
+from keras.callbacks import EarlyStopping
+from model import munch
 from data_creator import download_flickr_data
 
 
@@ -38,7 +38,7 @@ train_ds = train_ds.cache().prefetch(buffer_size=at)
 val_ds = val_ds.cache().prefetch(buffer_size=at)
 
 #Creating and training the model
-model = create_model()
+model = munch()
 
 model.compile(
     optimizer='adam',
@@ -46,14 +46,15 @@ model.compile(
     metrics=['accuracy']
 )
 
-checkpoint_dir = Path.cwd() / f'trained models/model1.ckpt'
-callback = ModelCheckpoint(
-    filepath=checkpoint_dir,
-    save_weights_only=True,
-    verbose=1
+earlystopping = EarlyStopping(
+    monitor="val_loss",
+    patience=3,
+    restore_best_weights=True
 )
 
-history = model.fit(train_ds, epochs=5, validation_data=val_ds, callbacks=[callback])
+history = model.fit(train_ds, epochs=50, validation_data=val_ds, callbacks=[earlystopping])
+
+model.save('trained models/model1')
 
 #Plotting accuracy
 plt.subplot(1, 2, 1)
--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, MaxPooling2D, Conv2D, Rescaling, Activation
+from keras.layers import Flatten, Dense, MaxPooling2D, Conv2D, Rescaling, Activation, Dropout
 
-def create_model():
+def munch():
     model = Sequential()
 
     model.add(Rescaling(1.0/255))
@@ -9,13 +9,15 @@ def create_model():
     model.add(Conv2D(32, 3, padding='same'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D())
-
+    
     model.add(Conv2D(64, 3, padding='same'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D())
 
     model.add(Flatten())
     model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.6))
+
     model.add(Dense(1, activation='sigmoid'))
 
     return model
